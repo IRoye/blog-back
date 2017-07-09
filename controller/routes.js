e35622de99b202e523ed0d27ee18efff39f6b182
roye yu <royeyu@royedeMacBook-Pro.local>
2017-07-09 17:31:01 +0800

    注册

--- controller/routes.js
+++ controller/routes.js
@@ -1,6 +1,7 @@
 
 let Cat = require('./cat');
 let Product = require('./product');
+let User = require('./user');
 module.exports = function (app) {
     //添加标签
     app.post('/cat', Cat.add);
@@ -12,4 +13,8 @@ module.exports = function (app) {
     // product 相关的信息
     // 新增product    
     app.post('/product/new', Product.new);
+
+
+    // 注册
+    app.post('/user/signup', User.signup);
 }
--- models/User.js
+++ models/User.js
@@ -1,34 +1,29 @@
 // 模型类， 通过操作模型类操作数据
 const  mongoose = require('mongoose');
 // 加密操作
-const bcrypt = require('bcrypy');
-const saltRounds = 10;
+const crypto = require('crypto');
+const key = "secret-key";
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var userSchema  = new mongoose.Schema({
     username: {type: String, maxlength:18},
-    password: String,
+    tel:{type: String, },
+    userpass: {type:String},
     role: {type: Number, default: 0},
 })
 
 // 中间件
 userSchema.pre('save', function(next){
     var user = this;
-
-    bcrypt.genSalt(SALT_WORK_FACTOR, function (err,salt) {
-    if (err) return next(err);
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
-    //  hash ， 保存加密后的结果
-      user.password = hash;
-      next()
-    })
-  })
+    console.log(user);
+    this.userpass = converPass(user.userpass, key);
+    console.log(JSON.stringify(user));
+    next();
 })
 
-bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
-    // res == true
-});
-
-
+function converPass(originalPass, secret){
+   return  crypto.createHmac('sha256', secret)
+                   .update(originalPass)
+                   .digest('hex');
+}
 module.exports = mongoose.model('User', userSchema);
