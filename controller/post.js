89e51bd036e83d77bfd483b28bc3dca45fbcee73
roye yu <royeyu@royedeMacBook-Pro.local>
2017-07-23 23:31:11 +0800

    完成文章编辑

--- controller/post.js
+++ controller/post.js
@@ -1,6 +1,7 @@
 // 存储文章
 
 let Post = require('../models/post.js');
+const mongoose = require('mongoose');
 
 // 新建文章
 exports.new = function (req, res) {
@@ -30,14 +31,16 @@ exports.getPosts = function(req, res){
 
 // 获取单篇文章
 exports.getPost = function(req, res){
-   let postId = req.query.postId
-   console.log('postObj:', postId);
-   Post.findOne({_id: postId}, function(err, post){
+   let postId = req.query.postId;
+   console.log('postObj:', req.query.postId);
+//    直接使用findById接口
+   Post.findById({_id: postId}, function(err, post){
+       console.log('返回的文章：', post);
         if(err) return res.json({err, msg: '获取文章失败', code: '3'})
          res.json({
              code: '0',
              msg: '获取文章成功',
-             post
+             post:post
          })    
    });
 }
